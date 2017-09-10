7de60cc8271f2ece2e1666fbf1f9332734814416
jasongurwin <jgurwin@JGURWIN-MAC.local>
2017-09-10 15:48:38 -0700

    Updated Minesweeper with print board function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,8 +1,29 @@
-const blankLine='  |   |  '
-const guessLine='1 |   |  '
-const bombLine='  | B |  '
+const printBoard = board => {
+    console.log('Current Board:');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
+// const blankLine='  |   |  '
+// const guessLine='1 |   |  '
+// const bombLine='  | B |  '
 
-console.log("This is what an empty board would look like:")
-console.log(guessLine)
-console.log(bombLine)
-console.log(blankLine)
+const board = [
+            [' ',' ',' '],
+            [' ',' ',' '],
+            [' ',' ',' ']
+           ];
+
+
+printBoard(board)
+
+board[0][1]='1'
+board[2][2]='B'
+
+printBoard(board)
+
+
+// console.log("This is what an empty board would look like:")
+// console.log(guessLine)
+// console.log(bombLine)
+// console.log(blankLine)
