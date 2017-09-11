a46cf33b6765f89d2a78c4cff91b8c58f000d202
jasongurwin <jgurwin@JGURWIN-MAC.local>
2017-09-10 19:49:00 -0700

    Part 3 of the Project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,29 +1,85 @@
-const printBoard = board => {
-    console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-};
+
 // const blankLine='  |   |  '
 // const guessLine='1 |   |  '
 // const bombLine='  | B |  '
 
-const board = [
-            [' ',' ',' '],
-            [' ',' ',' '],
-            [' ',' ',' ']
-           ];
+// const board = [
+//             [' ',' ',' '],
+//             [' ',' ',' '],
+//             [' ',' ',' ']
+//            ];
+//
+//
+// // printBoard(board)
+//
+// board[0][1]='1'
+// board[2][2]='B'
+
+// printBoard(board)
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+
+  let board=[];
+
+  for (let rowNumber = 1; rowNumber <= numberOfRows; rowNumber ++){
+    let row=[];
+
+    for (let colNumber = 1; colNumber <= numberOfColumns; colNumber ++){
+
+      row.push(' ');
+
+    }
+    board.push(row);
+  }
+  return board;
+  };
+
+const generateBombBoard = (numberOfRows,numberOfColumns,numberOfBombs) => {
+
+  let board=[];
+
+  for (let rowNumber = 1; rowNumber <= numberOfRows; rowNumber ++){
+    let row=[];
+
+    for (let colNumber = 1; colNumber <= numberOfColumns; colNumber ++){
+
+      row.push(null);
+
+    }
+    board.push(row);
+  }
+
+  let numberOfBombsPlaced=0
+
+  while (numberOfBombsPlaced<numberOfBombs){
+
+    //This may place bombs on top an existing board position
+
+    randomRowIndex=Math.floor(Math.random() * numberOfRows);
+    randomColumnIndex=Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnIndex]= 'B';
+
+    numberOfBombsPlaced++
+  };
 
 
-printBoard(board)
+  return board;
 
-board[0][1]='1'
-board[2][2]='B'
+};
 
-printBoard(board)
+let printBoard=(board) => {
+
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 
 
-// console.log("This is what an empty board would look like:")
-// console.log(guessLine)
-// console.log(bombLine)
-// console.log(blankLine)
+
+};
+
+let playerBoard=generatePlayerBoard(3,4)
+let bombBoard=generateBombBoard(3,4,5)
+
+console.log('Player Board:')
+printBoard(playerBoard)
+console.log('Bomb Board')
+printBoard(bombBoard)
