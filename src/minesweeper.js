2edd1b540047a85e9e79b5490edd89b70a13cedf
jasongurwin <jgurwin@JGURWIN-MAC.local>
2017-09-21 08:54:56 -0700

    Updated part 4 through setp 24

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -55,18 +55,58 @@ const generateBombBoard = (numberOfRows,numberOfColumns,numberOfBombs) => {
 
     //This may place bombs on top an existing board position
 
-    randomRowIndex=Math.floor(Math.random() * numberOfRows);
-    randomColumnIndex=Math.floor(Math.random() * numberOfColumns);
+    let randomRowIndex=Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex=Math.floor(Math.random() * numberOfColumns);
 
-    board[randomRowIndex][randomColumnIndex]= 'B';
+    if (board[randomRowIndex][randomColumnIndex] != 'B') {
 
-    numberOfBombsPlaced++
+      board[randomRowIndex][randomColumnIndex]= 'B';
+      numberOfBombsPlaced++;
+
+    } ;
+
+
   };
 
 
   return board;
 
 };
+
+
+const getNumberOfNeighborBombs = (bombBoard,rowIndex,columnIndex) => {
+
+  const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >=0 && neighborColumnIndex < numberOfColumns){
+
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B'){
+
+        numberOfBombs ++;
+
+
+
+      }
+
+
+    }
+
+    return numberOfBombs
+
+  });
+
+
+
+}
 
 let printBoard=(board) => {
 
